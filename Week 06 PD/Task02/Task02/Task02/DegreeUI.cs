ec6e2495beac33bb688abb03472fcd53f0f0eee5
Robass Atif <120154625+robassatif114118@users.noreply.github.com>
2024-01-05 19:23:37 +0500

    code

---
+++ Week 06 PD/Task02/Task02/Task02/DegreeUI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class DegreeUI
+    {
+        public static  ProgramOffer addProgram()
+        {
+            Console.WriteLine("Enter degree Title:");
+            string degreeTitle = Console.ReadLine();
+            Console.WriteLine("Enter degree Duration: ");
+            string degreeDuration = Console.ReadLine();
+            Console.WriteLine("Enter degree Seats:");
+            int seat = int.Parse(Console.ReadLine());
+
+            ProgramOffer s1 = new ProgramOffer(degreeTitle, degreeDuration,seat);
+            Console.WriteLine("How many subject you want to register?");
+            int count = int.Parse(Console.ReadLine());
+            for(int i=0;i<count;i++)
+            {
+                Console.WriteLine("\n\tSubject {0}\n ", (i+1));
+
+                s1.addSubject(SubjectUI.takeInputForSubject());
+            }
+            return s1;
+
+        }
+        public static void viewDegreeProgram()
+        {
+            foreach(ProgramOffer x in ProgramCrud.degreeList )
+            {
+                Console.WriteLine(x.degreeTitle);
+            }
+
+        }
+    }
+}
